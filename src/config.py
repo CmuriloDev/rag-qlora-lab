87127ba60a689bc1b19c4b1d9a4dd7010988b548
CmuriloDev <murilo.portela.dev@gmail.com>
2026-05-29 19:15:32 -0300

    fix: fix some project functions

--- src/config.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-MODEL_ID = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-
-MAX_NEW_TOKENS = 100
-
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
--- test.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-print(torch.cuda.is_available())
-print(torch.cuda.get_device_name(0))
-
-x = torch.randn(1000,1000).cuda()
-y = x @ x
-
-print(y.shape)
